a19a80501f6a3193bc920cf3ade2ff70a8f7b83a
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-26 16:03:09 +0100

    add third snack

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,3 +2,7 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+/* map() */
+const newNumbers = numbers.map(numb => numb + 1)
+console.log(newNumbers);
