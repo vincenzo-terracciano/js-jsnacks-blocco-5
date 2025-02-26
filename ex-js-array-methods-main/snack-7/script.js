126fa88a222de3fd366fe0fbcf3830782bfd0fc3
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-26 16:20:38 +0100

    add snack 7

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,12 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+/* find() */
+const findStudent = students.find((number) => {
+  if(number.id === 2){
+    return true
+  }
+  return false
+})
+console.log(findStudent);
