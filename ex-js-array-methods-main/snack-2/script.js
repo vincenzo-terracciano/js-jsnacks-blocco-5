f45fe66ce1e3abcfd9595c947f4f4e7547be1fe9
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-26 15:58:56 +0100

    add second snack

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -6,3 +6,9 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+/* forEach */
+people.forEach((firstName) => {
+  console.log(firstName.name);
+  
+})
