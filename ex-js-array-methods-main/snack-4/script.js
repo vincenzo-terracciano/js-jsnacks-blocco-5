de99bd3d34ffea83416c430f795f93a88e6f2ce8
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-26 16:11:35 +0100

    add fifth snack

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -8,7 +8,7 @@ const posts = [
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
 
-/* filter() */
+/* map() */
 const allAuthors = posts.map((nameAuthor) => {
   return nameAuthor.author
 })
--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,9 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+/* filter() */
+const evenNumbers = nums.filter(numb => numb % 2 === 0)
+console.log(evenNumbers);
+
+
